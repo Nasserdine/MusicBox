e27cb2234a71e0a525c67e987a22cc1bcc85e154
Nasserdine <nasserdine.bouguerne@gmail.com>
2017-02-17 15:43:40 +0100

    Ajout du projet

---
+++ musicBox.js
@@ -0,0 +1,36 @@
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
+var p3 = document.getElementById('p3');
+var p4 = document.getElementById('p4');
+var p5 = document.getElementById('p5');
+var p6 = document.getElementById('p6');
+
+var s1 = document.getElementById('son1');
+var s2 = document.getElementById('son2');
+var s3 = document.getElementById('son3');
+var s4 = document.getElementById('son4');
+var s5 = document.getElementById('son5');
+var s6 = document.getElementById('son6');
+
+p1.onclick = function(){
+	s1.play();
+}
+
+p2.onclick = function(){
+	s2.play();
+}
+
+p3.onclick = function(){
+	s3.play();
+}
+
+p4.onclick = function(){
+	s4.play();
+}
+
+p5.onclick = function(){
+	s5.play();
+}
+p6.onclick = function(){
+	s6.play();
+}
